b8dcd804ad375573d52ce94770380cf4dd437449
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-06 15:55:39 +0700

    SystemTime

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SYSTEMTIME;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine("Datetime now is: "+ localDate);
+        }
+    
+
+    }
+    
+
+
